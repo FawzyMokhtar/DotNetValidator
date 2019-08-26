5dc14dbe4176b494759d8393333298c823e4214b
FawzyMokhtar <dev.fawzymokhtar@gmail.com>
2019-08-26 06:42:19 +0200

    Remove the unnecessary overloads from the custom-validator validation method & the custom-sanitizer sanitization method, since both are accepting a generic type and there is no need for any overloads that takes a static type.

--- DotNetValidator.Framework/Utilities/Validators/CustomValidatorFunc.cs
+++ DotNetValidator.Framework/Utilities/Validators/CustomValidatorFunc.cs
@@ -6,31 +6,6 @@ namespace DotNetValidator
     public static partial class ValidationUtility
     {
         /// <summary>
-        /// Creates a custom validation method that accepts an object value and 
-        /// returns true if there is no validation error, otherwise false.
-        /// <para>Any mutation to the value will not affect the original value.</para>
-        /// </summary>
-        /// <param name="validator">The validator to add more validations or sanitization.</param>
-        /// <param name="customValidator">A custom validator that accepts an object value and 
-        /// returns true if there is no validation error, otherwise false.</param>
-        /// <returns>A Validator.</returns>
-        public static Validator CustomValidator(this Validator validator, Func<object, bool> customValidator,
-            string errorMessage)
-        {
-            try
-            {
-                // pass a deep clone copy to avoid data mutation
-                if(!customValidator(Serializer.Deserialize<object>(Serializer.Serialize(validator.GetValue()))))
-                    validator.AddError(errorMessage);
-            }
-            catch (Exception)
-            {
-                validator.AddError(errorMessage);
-            }
-            return validator;
-        }
-
-        /// <summary>
         /// Creates a custom validation method that accepts a TSource value and 
         /// returns true if there is no validation error, otherwise false.
         /// <para>Any mutation to the value will not affect the original value.</para>
