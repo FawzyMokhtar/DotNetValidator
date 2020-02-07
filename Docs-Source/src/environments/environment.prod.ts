026e6b7c160538e9f5956554e55993cb71c28bbb
FawzyMokhtar <dev.fawzymokhtar@gmail.com>
2020-02-08 01:42:17 +0200

    Build docs.

--- Docs-Source/src/environments/environment.prod.ts
+++ Docs-Source/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  gitHubRepo: "https://github.com/DotNetValidator/DotNetValidator",
-  gitHubApi: "https://api.github.com/repos/DotNetValidator/DotNetValidator",
+  gitHubRepo: "https://github.com/FawzyMokhtar/DotNetValidator",
+  gitHubApi: "https://api.github.com/repos/FawzyMokhtar/DotNetValidator",
   nugetPackage: "https://www.nuget.org/packages/DotNetValidator/",
   nugetApi: "https://api-v2v3search-0.nuget.org/query?q=packageid:dotnetvalidator",
   stackOverflow: "https://stackoverflow.com/questions/tagged/DotNetValidator",
